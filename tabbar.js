e5586b8cd592a93a108d9c94b7ed2a631049b65e
Jeffrey Su <sujeff@wustl.edu>
2020-10-30 15:27:14 -0500

    Styled home

--- tabbar.js
+++ tabbar.js
@@ -1,14 +1,11 @@
-const tabs = document.querySelectorAll('.tab-bar-item');
-const tabsPages = document.querySelectorAll('.tab-page');
-tabs.forEach(clickedTab => {
-    clickedTab.addEventListener('click', () => {
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
-            // tabsPages.classList.remove('active-tab')
-        });
+// const tabs = document.querySelectorAll('.tab-bar-item');
+// tabs.forEach(clickedTab => {
+//     clickedTab.addEventListener('click', () => {
+//         tabs.forEach(tab => {
+//             tab.classList.remove('active')
+//         });
 
-        clickedTab.classList.add('active');
-        // tabsPages.classList.add('active-tab');
+//         clickedTab.classList.add('active');
 
-    })
-})
+//     })
+// })
