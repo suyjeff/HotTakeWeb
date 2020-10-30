ffe4148f9107e96cfab173a2ab08fa6252d1ba8f
Jeffrey Su <sujeff@wustl.edu>
2020-10-30 14:29:04 -0500

    Added stuff

--- tabbar.js
+++ tabbar.js
@@ -1,11 +1,14 @@
 const tabs = document.querySelectorAll('.tab-bar-item');
+const tabsPages = document.querySelectorAll('.tab-page');
 tabs.forEach(clickedTab => {
     clickedTab.addEventListener('click', () => {
         tabs.forEach(tab => {
             tab.classList.remove('active')
+            // tabsPages.classList.remove('active-tab')
         });
 
         clickedTab.classList.add('active');
+        // tabsPages.classList.add('active-tab');
 
     })
 })
