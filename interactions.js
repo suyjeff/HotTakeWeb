2d4cd566c9fe9a299fab75f54e78c6d84e693ef3
Ryan Miller <ryanvmiller@icloud.com>
2020-11-21 21:02:56 -0600

    Added display name to create account Added loading to join Added functionality to user page - profile picture, email, display name, team, change team, change display name, change avatar

--- interactions.js
+++ interactions.js
@@ -1,2 +1,5 @@
-$(".loader").delay(1800).fadeOut(300);
-$(".tab-page").delay(1850).fadeIn(300);
+document.addEventListener("DOMContentLoaded", pageLoaded, false);
+function pageLoaded() {
+    $(".loader").delay(1800).fadeOut(300);
+    $(".tab-page").delay(1850).fadeIn(300);
+}
