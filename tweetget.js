c912a6b0905be1a34e630ad7af485cc946e2c258
Jeffrey Su <sujeff@wustl.edu>
2020-11-23 20:38:11 -0600

    (1) Push commit

--- tweetget.js
+++ tweetget.js
@@ -1,4 +1,4 @@
-const bearer_token = "";
+const bearer_token = "AAAAAAAAAAAAAAAAAAAAAEQDKAEAAAAAMys%2B5ZlRaejq8LfOrgQBVbLLX%2Fo%3DJbyTyyXAAC7Uus9WSD7sY22xA9x1xUzbYamJprkwjNIsocfhvg";
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -9,12 +9,12 @@ xhttp.onreadystatechange = function() {
         for (var i=0; i<storylines.length; i++) {
             createStoryline(storylines[i]);
         }
-
     }
 };
 
-xhttp.open("GET", "https://api.twitter.com/2/tweets/search/recent?query=", true);
+xhttp.open("GET", "https://api.twitter.com/2/tweets/search/recent?query=jeff", true);
 xhttp.setRequestHeader("authorization", "Bearer ${bearer_token}");
+xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
 xhttp.send();
 
 function createStoryline(storyline) {
